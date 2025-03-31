0f2b37a2e5b4ddb102bad35ccbb5edbbaa18574c
KrushnaBaral <krushna.baral@lagozon.com>
2025-03-31 10:52:05 +0530

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,4 +1,3 @@
-
 from examples import get_example_selector
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder,FewShotChatMessagePromptTemplate,PromptTemplate # type: ignore
 import os
@@ -6,14 +5,8 @@ from dotenv import load_dotenv # type: ignore
 
 load_dotenv()  # Load environment variables from .env file
 
-# Read the SQL prompt from a text file instead of .env
-def load_prompt():
-    with open("final_prompt.txt", "r", encoding="utf-8") as file:
-        return file.read()
-
-FINAL_PROMPT = load_prompt()
 # Get the static part of the prompt
-static_prompt = FINAL_PROMPT
+static_prompt = os.getenv("FINAL_PROMPT")
 example_prompt = ChatPromptTemplate.from_messages(
     [
         # ("human", "{input}\nSQLQuery:"),
@@ -27,7 +20,7 @@ few_shot_prompt = FewShotChatMessagePromptTemplate(
     input_variables=["input","top_k"],
 )
 
-final_prompt = ChatPromptTemplate.from_messages(
+final_prompt2 = ChatPromptTemplate.from_messages(
     [
         ("system", static_prompt.format(table_info="{table_info}")),
         few_shot_prompt,
@@ -39,7 +32,7 @@ final_prompt = ChatPromptTemplate.from_messages(
 answer_prompt = PromptTemplate.from_template(
     """Given the user question, corresponding SQL query, and SQL result, answer the user question.
      Start with SQL query as first line of your answer then follow it with your answer in new line.
-     Respond without modifying any of the nouns or numerical values. 
+     Respond without modifying any of the nouns or numerical values.
      DO NOT modify any of the nouns or numerical values received in SQL result.
 
 Question: {question}
